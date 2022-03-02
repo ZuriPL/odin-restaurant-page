85f1973bc314e821d95d717c3a91ca5fd5e1d756
zuriii <michal.bak222@gmail.com>
2022-03-02 19:11:12 +0100

    changed line-height for home

--- src/components/home.js
+++ src/components/home.js
@@ -7,7 +7,8 @@ export default function() {
     home.innerHTML = `
         <h2 class="header"><span style="color: var(--accent-color)">H</span>ome</h2>
         <p class="home-text">
-        I'm too lazy to finish it up, sorry<br><br>
+        I'm too lazy to finish it up, sorry<br>
+        The whole site works of course, just some placeholders here<br><br>
 
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto in eaque vel assumenda, facilis ipsam. Incidunt recusandae quibusdam ab, ad quod sint accusamus. Corporis accusamus repellat error animi nisi ipsam veritatis praesentium in tempore nam repellendus ipsum, suscipit perferendis fugiat!Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto in eaque vel assumenda, facilis ipsam. Incidunt recusandae quibusdam ab, ad quod sint accusamus. Corporis accusamus repellat error animi nisi ipsam veritatis praesentium in tempore nam repellendus ipsum, suscipit perferendis fugiat!Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto in eaque vel assumenda, facilis ipsam. Incidunt recusandae quibusdam ab, ad quod sint accusamus. Corporis accusamus repellat error animi nisi ipsam veritatis praesentium in tempore nam repellendus ipsum, suscipit perferendis fugiat!Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto in eaque vel assumenda, facilis ipsam. Incidunt recusandae quibusdam ab, ad quod sint accusamus. Corporis accusamus repellat error animi nisi ipsam veritatis praesentium in tempore nam repellendus ipsum, suscipit perferendis fugiat!
         </p>
